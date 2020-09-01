7722250f4904e7ac3d4dcd95c63be65d1f5c5470
Samuel Mehrbrodt <Samuel.Mehrbrodt@cib.de>
2020-09-01 11:27:37 +0200

    Remove unused code

--- barcode/Barcode.py
+++ barcode/Barcode.py
@@ -11,7 +11,7 @@ import os
 import uno, unohelper
 from com.sun.star.task import XJobExecutor
 from com.sun.star.document import XEventListener
-from com.sun.star.awt import XMouseListener, XActionListener
+from com.sun.star.awt import XActionListener
 from com.sun.star.text.TextContentAnchorType import AT_PARAGRAPH
 
 import draw
@@ -25,7 +25,7 @@ com_sun_star_awt_SystemPointer_ARROW = uno.getConstantByName( 'com.sun.star.awt.
 com_sun_star_awt_SystemPointer_REFHAND = uno.getConstantByName( 'com.sun.star.awt.SystemPointer.REFHAND' )
 com_sun_star_drawing_TextHorizontalAdjust_CENTER = 1
 
-class Barcode( ComponentBase, XMouseListener, XActionListener ):
+class Barcode( ComponentBase, XActionListener ):
     SUPPORTED_LANGUAGES = SUPPORTED_LANGUAGES
 
     def insert( self ):
@@ -561,37 +561,12 @@ class Barcode( ComponentBase, XMouseListener, XActionListener ):
             return self.checksum_UPCA( code )
         else:
             return self.checksum_UPCA( '0' + code )
-
-    def showabout( self ):
-        self.dlg = dlg = self.createdialog( 'About' )
-        if DEBUG:
-            dlg.DebugButton.addActionListener( self )
-        else:
-            dlg.DebugButton.setVisible( False )
-        dlg.Image.Model.ImageURL = unohelper.systemPathToFileUrl( self.path + '/barcode_about.png' )
-        dlg.URL.addMouseListener( self )
-        dlg.execute()
-    def on_action_DebugButton( self ):
-        self.dlg.endExecute()
-        self.dlg = dlg = self.createdialog( 'ExtensionCreator' )
-        dlg.ExecuteCode.addActionListener( self )
-        dlg.SaveDialogs.addActionListener( self )
-        self.updateOutputInCreatorDialog()
-        dlg.execute()
-    def updateOutputInCreatorDialog( self ):
-
-        if sys.platform in DEBUGFILEPLATFORMS:
-            self.dlg.OutputField.Model.Text = file( debugfile, 'r' ).read()
-            selection = uno.createUnoStruct( 'com.sun.star.awt.Selection' )
-            selection.Min = selection.Max = len( self.dlg.OutputField.Model.Text )
-            self.dlg.OutputField.Selection = selection
     def on_action_ExecuteCode( self ):
         try:
             code = self.dlg.CodeField.Model.Text
             exec(code)
         except:
             debugexception()
-        self.updateOutputInCreatorDialog()
     def on_action_SaveDialogs( self ):
         try:
             dialogs = 'EOECBarcodeDialogs'
@@ -604,40 +579,10 @@ class Barcode( ComponentBase, XMouseListener, XActionListener ):
             debug( 'dialogs saved' )
         except:
             debugexception()
-        self.updateOutputInCreatorDialog()
     # XActionListener
     def actionPerformed( self, event ):
         try:
             getattr( self, 'on_action_' + event.Source.Model.Name )()
-        except:
-            debugexception()
-    # XMouseListener
-    def mousePressed( self, event ):
-        pass
-    def mouseReleased( self, event ):
-        try:
-            shellexec = self.ctx.getServiceManager().createInstanceWithContext( 'com.sun.star.system.SystemShellExecute', self.ctx )
-            shellexec.execute( self.localize( 'aboutURL' ), '', uno.getConstantByName( 'com.sun.star.system.SystemShellExecuteFlags.DEFAULTS' ) )
-        except:
-            debugexception()
-    def mouseEntered( self, event ):
-        try:
-            peer = self.dlg.Peer
-            pointer = self.ctx.getServiceManager().createInstance( 'com.sun.star.awt.Pointer' )
-            pointer.setType( com_sun_star_awt_SystemPointer_REFHAND )
-            peer.setPointer( pointer )
-            for w in peer.Windows:
-                w.setPointer( pointer )
-        except:
-            debugexception()
-    def mouseExited( self, event ):
-        try:
-            peer = self.dlg.Peer
-            pointer = self.ctx.getServiceManager().createInstance( 'com.sun.star.awt.Pointer' )
-            pointer.setType( com_sun_star_awt_SystemPointer_ARROW )
-            peer.setPointer( pointer )
-            for w in peer.Windows:
-                w.setPointer( pointer )
         except:
             debugexception()
 
